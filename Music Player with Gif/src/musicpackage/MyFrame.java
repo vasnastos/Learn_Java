52b6acfeac2c0fc7f5db55c20819541644657c11
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2020-09-14 12:30:01 +0300

    Add files via upload

--- Music Player with Gif/src/musicpackage/MyFrame.java
+++ Music Player with Gif/src/musicpackage/MyFrame.java
@@ -1,5 +1,6 @@
 package musicpackage;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -9,32 +10,48 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
+import java.util.Vector;
 
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
+import javax.swing.JTable;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TableColumn;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 public class MyFrame extends JFrame{
    JPanel p1,p2;
    MediaPlayer player=null;
    JLabel gifsong;
+   JSlider duration;
    String song="";
    double volume=0.10;
    boolean muted=false;
+   boolean paused=true;
+   JButton b1=new JButton();
+   JList <String> lst=new JList<String>();
+   DefaultListModel model=null;
    void makemenu()
    {
 	   JMenuBar bar=new JMenuBar();
@@ -50,13 +67,8 @@ public class MyFrame extends JFrame{
 			   int rval=ch.showOpenDialog(MyFrame.this);
 			   if(rval==JFileChooser.APPROVE_OPTION)
 			   {
-				   String fn=ch.getSelectedFile().getAbsolutePath();
-				   File fp=new File(fn);
-				   song=fn;
-				   String filepath=fp.toURI().toString();
-				   Media med=new Media(filepath);
-				   player=new MediaPlayer(med);
-				   player.setVolume(volume);
+				   String f=ch.getSelectedFile().getAbsolutePath();
+				   model.addElement(f);
 			   }
 		   }
 	   });
@@ -71,8 +83,7 @@ public class MyFrame extends JFrame{
 				   JOptionPane.showMessageDialog(null,"No song is currently played");
 				   return;
 			   }
-			   String data[]=song.split("/");
-			   JOptionPane.showMessageDialog(null,"Song Played:"+data[data.length-1]);
+			   JOptionPane.showMessageDialog(null,"Song Played:"+lst.getSelectedValue());
 		   }
 	   });
 	   menu.add(it1);
@@ -84,7 +95,7 @@ public class MyFrame extends JFrame{
 	   p2=new JPanel();
 	   p2.setLayout(new FlowLayout());
 	   gifsong=new JLabel();
-	   gifsong.setSize((int)(0.98*this.getWidth()),(int)(0.4*this.getHeight()));
+	   gifsong.setSize((int)(0.98*this.getWidth()),(int)(0.3*this.getHeight()));
 	   ImageIcon icon=new ImageIcon("music.gif");
 	   icon.setImage(icon.getImage().getScaledInstance(gifsong.getWidth(),gifsong.getHeight(),Image.SCALE_REPLICATE));
 	   gifsong.setIcon(icon);
@@ -95,28 +106,27 @@ public class MyFrame extends JFrame{
    {
 	   p1=new JPanel();
 	   p1.setLayout(new FlowLayout());
-	   JButton b1=new JButton("Play");
-	   JButton b2=new JButton("Pause");
+	   b1.setText("Play");
 	   b1.addActionListener(new ActionListener()
-		{
+	    {
 		   public void actionPerformed(ActionEvent e)
 		   {
-			   if(player!=null)
+			   paused=!paused;
+			   if(!paused && player!=null)
 			   {
 				   player.play();
+				   b1.setText("Pause");
 			   }
-		   }
-		});
-	   b2.addActionListener(new ActionListener()
-	   {
-		   public void actionPerformed(ActionEvent e)
-		   {
-			   if(player!=null)
+			   else
 			   {
-				   player.pause();
+				   if(player!=null)
+				   {
+					   player.pause();
+					   b1.setText("Play");
+				   }
 			   }
 		   }
-	   });
+	    });
 	   JSlider slider=new JSlider(JSlider.HORIZONTAL);
 	   slider.setMaximum(100);
 	   slider.setMinimum(0);
@@ -132,6 +142,34 @@ public class MyFrame extends JFrame{
 			}
           
 	    });
+	   JButton next=new JButton("Next");
+	   next.addActionListener(new ActionListener()
+	   {
+		   public void actionPerformed(ActionEvent e)
+		   {
+			   if(player!=null)
+			   {
+				   player.stop();
+			   }
+			   int i=lst.getSelectedIndex();
+			   System.out.print(lst.getLastVisibleIndex());
+			   if(i==lst.getLastVisibleIndex())
+			   {
+			    i=0;
+			   }
+			   else
+			   {
+				   i++;
+			   }
+			   lst.setSelectedIndex(i);
+			   File fp=new File(lst.getSelectedValue());
+			   String filepath=fp.toURI().toString();
+			   Media m=new Media(filepath);
+			   player=new MediaPlayer(m);
+			   player.play();
+			   player.setVolume(volume);
+		   }
+	   });
 	   JButton b3=new JButton("Mute");
 	   b3.addActionListener(new ActionListener()
 	   {
@@ -141,12 +179,40 @@ public class MyFrame extends JFrame{
 			   player.setMute(muted);
 		   }
 	   });
+	   JButton prev=new JButton("Prev");
+	   prev.addActionListener(new ActionListener()
+	   {
+		   public void actionPerformed(ActionEvent e)
+		   {
+			   if(player!=null)
+			   {
+				   player.stop();
+			   }
+			   int i=lst.getSelectedIndex();
+			   if(i==0)
+			   {
+				   i=lst.getLastVisibleIndex();
+			   }
+			   else
+			   {
+				   i--;
+			   }
+			   File fp=new File(lst.getSelectedValue());
+			   String filepath=fp.toURI().toString();
+			   Media m=new Media(filepath);
+			   player=new MediaPlayer(m);
+			   player.setVolume(volume);
+			   player.play();
+		   }
+	   });
 	   p1.add(b1);
-	   p1.add(b2);
 	   p1.add(slider);
+	   p1.add(next);
 	   p1.add(b3);
+	   p1.add(prev);
 	   this.add(p1);
    }
+
    MyFrame()
    {
 	   this.setSize(500,400);
@@ -155,6 +221,52 @@ public class MyFrame extends JFrame{
 	   this.setLayout(new GridLayout(0,1));
 	   makemenu();
 	   Gifpanel();
+	   lst.setSize((int)(0.95*this.getWidth()),(int)(0.47*this.getHeight()));
+       model=new DefaultListModel();
+       lst.setAlignmentX(CENTER_ALIGNMENT);
+       lst.setModel(model);
+       lst.addListSelectionListener(new ListSelectionListener()
+       {
+    	   public void valueChanged(ListSelectionEvent arg0) {
+					String k=lst.getSelectedValue();
+					if(player!=null)
+					{
+						player.stop();
+					}
+					File fp=new File(k);
+					String filepath=fp.toURI().toString();
+					Media md=new Media(filepath);
+					player=new MediaPlayer(md);
+					player.setVolume(volume);
+					 player.setOnEndOfMedia(new Runnable()
+			          {
+			            @Override
+					     public void run() {
+					       int index=lst.getSelectedIndex();
+					       if(index==lst.getLastVisibleIndex())
+					       {
+					    	  index=0;
+					       }
+					       else
+					       {
+					    	   index++;
+					       }
+					       lst.setSelectedIndex(index);
+					       System.out.println(lst.getSelectedIndex());
+					       String filepath=lst.getSelectedValue();
+					       File fp=new File(filepath);
+					       String uriformat=fp.toURI().toString();
+					       Media md=new Media(uriformat);
+					       player=new MediaPlayer(md);
+					       player.setVolume(volume);
+					       player.play();
+					       b1.setText("Pause");
+					}
+			       });
+       }
+       });
+	   JScrollPane pane=new JScrollPane(lst);
+	   this.add(pane);
 	   songoptions();
 	   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	   this.setVisible(true);
